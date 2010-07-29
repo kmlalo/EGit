3093ddb05fed4f6c1f478752de4a8acf7421d500
김광기 <@pcsw-kkg>
2010-07-29 21:41:09 +0900

    add new brancheone ... System.out.println(" Hello Tow New Branch");

--- src/HelloTwoNewBranch.java
+++ src/HelloTwoNewBranch.java
@@ -7,6 +7,7 @@ public class HelloTwoNewBranch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("add HelloTwoNewBranch");
+		System.out.println("add new branchone HelloTwoNewBranch");
 	}
 
 }
